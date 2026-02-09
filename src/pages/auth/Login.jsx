e797fb0ac2185cb303a4a49354f6afc9b825d5e3
Wissal <wissal1zabour@gmail.com>
2026-02-09 14:15:41 +0100

    login full style

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -3,26 +3,38 @@ import '../../styles/index.css'
 
 function Login() {
   return (
-    <div class='bg-gradient-to-r from-gray-100 to-gray-600 h-screen flex justify-center items-center  flex-col '>
-        <h3>Admin space</h3>
-        <div class='bg-white w-2/5 h-3/5 rounded-2xl shadow-xl  p-10 flex flex-col justify-center items-center gap-5 '>
-           <h3>welcome back</h3>
-           <p>please enter your details</p>
-           <form action="">
-            <label ></label>
-             <input type="email" id='email' placeholder='Please enter your email'/>
-             <input type="password" id='pass' placeholder='Please enter your password'/>
-           </form>
-           <div>
-                <div>
+    <div className='bg-gradient-to-r from-gray-100 to-gray-600 min-h-screen flex justify-center items-center  flex-col '>
+      {/* card */}
+        <div className='bg-white w-full max-w-md rounded-2xl shadow-xl  px-8 py-10 flex flex-col justify-center items-center gap-6 '>
+             {/* titre */}
+             <div className='text-center'>
+               <h2 className='text-3xl font-bold  text-gray-00'>Admin space</h2>
+               <p className=' text-sm text-gray-500  mt-1'>welcome back! please enter your details</p>
+             </div>
+             {/* form */}
+           <form className='flex flex-col gap-4'>
+             {/* email */}
+             <div className='flex flex-col gap-2'>
+              <label className='text-sm text-gray-600'>Email</label>
+             <input className='px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500' type="email" id='email' placeholder='Please enter your email'/>
+             </div>
+              {/* password */}
+             <div className='flex flex-col gap-2'>
+               <label className='text-sm text-gray-600' >Password</label>
+               <input className='px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500' type="password" id='pass' placeholder='Please enter your password'/>
+             </div>
+             {/* options */}
+             <div className='flex flex-row justify-between gap-6 text-sm text-gray-500'>
+              <div className=' flex flex-row gap-2 items-center'>
                 <input type="checkbox" name="remember-me" id="rem" />
-                <label for="rem">Remember me</label>
-                </div>
-                <div>
-                   <h5>forgot password?</h5> 
-                </div>
-           </div>
-           <button>Login</button>
+                <label htmlFor="rem">Remember me</label>
+              </div>
+              <div>
+                   <button type='button' className='cursor-pointer hover:underline transition duration-500 '>forgot password?</button> 
+              </div>
+             </div>
+             <button className='cursor-pointer mt-4  py-2 bg-gray-800 border rounded-2xl font-semibold text-white hover:bg-gray-700 transition duration-200 '>Login</button>
+           </form>
         </div>
     </div>
   )
