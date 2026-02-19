f40be87fb059195b0e1a2d4bbef3c4d7c5ee8b90
Wissal <wissal1zabour@gmail.com>
2026-02-19 14:15:33 +0100

    hosting

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import '../../styles/index.css'
-import authService from '../../services/authService.js'
+import authService from '../../services/AuthService.js'
 
 function Register() {
   const [name, setName] = useState('')
